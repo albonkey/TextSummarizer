7260213dcf3c3b3cdebb0ec18ab85c62872e85b4
ali <a_icam@yahoo.com>
2023-05-15 18:29:52 -0700

    feat: Seq2Seq model added

--- model.py
+++ model.py
@@ -1,123 +1,118 @@
 # Import libraries
-from trax import layers as tl
+import tensorflow as tf
+from tensorflow.keras import Input, Model
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.layers import LSTM, Bidirectional, Dense, Embedding, TimeDistributed
 
-from attention import CausalAttention
+def build_seq2seq_model_with_just_lstm(
+    x_embedding_matrix, y_embedding_matrix,
+    latent_dim = 240, max_text_len=4096, embedding_dim = 300,
+    x_vocab_size=33300, y_vocab_size=33300,
+):
+    # instantiating the model in the strategy scope creates the model on the TPU
 
-def DecoderBlock(d_model, d_ff, n_heads, dropout, mode, ff_activation):
+    # =====================
+    # 🔥 Encoder
+    # =====================
+    encoder_input = Input(shape=(max_text_len, ))
 
-    """Returns a list of layers that implements a Transformer decoder block.
+    # encoder embedding layer
+    encoder_embedding = Embedding(
+        x_vocab_size,
+        embedding_dim,
+        embeddings_initializer=tf.keras.initializers.Constant(x_embedding_matrix),
+        trainable=False
+    )(encoder_input)
 
-    The input is an activation tensor.
-
-    Args:
-        d_model (int):  depth of embedding.
-        d_ff (int): depth of feed-forward layer.
-        n_heads (int): number of attention heads.
-        dropout (float): dropout rate (how much to drop out).
-        mode (str): 'train' or 'eval'.
-        ff_activation (function): the non-linearity in feed-forward layer.
-
-    Returns:
-        list: list of trax.layers.combinators.Serial that maps an activation tensor to an activation tensor.
-    """
-    
-    ### START CODE HERE (REPLACE INSTANCES OF 'None' WITH YOUR CODE) ###
-    
-    # Create masked multi-head attention block using CausalAttention function
-    causal_attention = CausalAttention( 
-                        d_model,
-                        n_heads=n_heads,
-                        mode=mode
-                        )
-
-    # Create feed-forward block (list) with two dense layers with dropout and input normalized
-    feed_forward = [ 
-        # Normalize layer inputs
-        tl.LayerNorm(),
-        # Add first feed forward (dense) layer (don't forget to set the correct value for n_units)
-        tl.Dense(d_ff),
-        # Add activation function passed in as a parameter (you need to call it!)
-        ff_activation(), # Generally ReLU
-        # Add dropout with rate and mode specified (i.e., don't use dropout during evaluation)
-        tl.Dropout(rate=dropout, mode=mode),
-        # Add second feed forward layer (don't forget to set the correct value for n_units)
-        tl.Dense(d_model),
-        # Add dropout with rate and mode specified (i.e., don't use dropout during evaluation)
-        tl.Dropout(rate=dropout, mode=mode)
-    ]
-
-    # Add list of two Residual blocks: the attention with normalization and dropout and feed-forward blocks
-    return [
-      tl.Residual(
-          # Normalize layer input
-          tl.LayerNorm(),
-          # Add causal attention block previously defined (without parentheses)
-          causal_attention,
-          # Add dropout with rate and mode specified
-          tl.Dropout(rate=dropout, mode=mode)
-        ),
-      tl.Residual(
-          # Add feed forward block (without parentheses)
-          feed_forward
-        ),
-      ]
-
-def TransformerLM(vocab_size=33300,
-                  d_model=512,
-                  d_ff=2048,
-                  n_layers=6,
-                  n_heads=8,
-                  dropout=0.1,
-                  max_len=4096,
-                  mode='train',
-                  ff_activation=tl.Relu):
-    """Returns a Transformer language model.
-
-    The input to the model is a tensor of tokens. (This model uses only the
-    decoder part of the overall Transformer.)
-
-    Args:
-        vocab_size (int): vocab size.
-        d_model (int):  depth of embedding.
-        d_ff (int): depth of feed-forward layer.
-        n_layers (int): number of decoder layers.
-        n_heads (int): number of attention heads.
-        dropout (float): dropout rate (how much to drop out).
-        max_len (int): maximum symbol length for positional encoding.
-        mode (str): 'train', 'eval' or 'predict', predict mode is for fast inference.
-        ff_activation (function): the non-linearity in feed-forward layer.
-
-    Returns:
-        trax.layers.combinators.Serial: A Transformer language model as a layer that maps from a tensor of tokens
-        to activations over a vocab set.
-    """
-    # Embedding inputs and positional encoder
-    positional_encoder = [ 
-        # Add embedding layer of dimension (vocab_size, d_model)
-        tl.Embedding(vocab_size, d_model),
-        # Use dropout with rate and mode specified
-        tl.Dropout(rate=dropout, mode=mode),
-        # Add positional encoding layer with maximum input length and mode specified
-        tl.PositionalEncoding(max_len=max_len, mode=mode)]
-
-    # Create stack (list) of decoder blocks with n_layers with necessary parameters
-    decoder_blocks = [ 
-        DecoderBlock(d_model, d_ff, n_heads, dropout, mode, ff_activation) for _ in range(n_layers)]
-
-    # Create the complete model as written in the figure
-    return tl.Serial(
-        # Use teacher forcing (feed output of previous step to current step)
-        tl.ShiftRight(mode=mode), # Specify the mode!
-        # Add positional encoder
-        positional_encoder,
-        # Add decoder blocks
-        decoder_blocks,
-        # Normalize layer
-        tl.LayerNorm(),
-
-        # Add dense layer of vocab_size (since need to select a word to translate to)
-        # (a.k.a., logits layer. Note: activation already set by ff_activation)
-        tl.Dense(vocab_size),
-        # Get probabilities with Logsoftmax
-        tl.LogSoftmax()
+    # encoder lstm 1
+    encoder_lstm1 = LSTM(
+        latent_dim,
+        return_sequences=True,
+        return_state=True,
+        dropout=0.4,
+        recurrent_dropout=0.4
     )
+    encoder_output1, state_h1, state_c1 = encoder_lstm1(encoder_embedding)
+
+    # encoder lstm 2
+    encoder_lstm2 = LSTM(
+        latent_dim,
+        return_sequences=True,
+        return_state=True,
+        dropout=0.4,
+        recurrent_dropout=0.4
+    )
+    encoder_output, *encoder_final_states = encoder_lstm2(encoder_output1)
+
+    # =====================
+    # 🌈 Decoder
+    # =====================
+
+    # Set up the decoder, using `encoder_states` as initial state.
+
+    decoder_input = Input(shape=(None, ))
+
+    # decoder embedding layer
+    decoder_embedding_layer = Embedding(
+        y_vocab_size,
+        embedding_dim,
+        embeddings_initializer=tf.keras.initializers.Constant(y_embedding_matrix),
+        trainable=True
+    )
+    decoder_embedding = decoder_embedding_layer(decoder_input)
+
+    # decoder lstm 1
+    decoder_lstm = LSTM(
+        latent_dim,
+        return_sequences=True,
+        return_state=True,
+        dropout=0.4,
+        recurrent_dropout=0.4
+    )
+    decoder_output, *decoder_final_states = decoder_lstm(
+        decoder_embedding, initial_state=encoder_final_states
+    )
+
+    # dense layer
+    decoder_dense = TimeDistributed(
+        Dense(y_vocab_size, activation='softmax')
+    )
+    decoder_output = decoder_dense(decoder_output)
+
+    # =====================
+    # ⚡️ Model
+    # =====================
+    model = Model([encoder_input, decoder_input], decoder_output)
+    model.summary()
+
+    optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.001)
+    model.compile(
+        optimizer=optimizer,
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    return {
+        'model': model,
+        'inputs': {
+            'encoder': encoder_input,
+            'decoder': decoder_input
+        },
+        'outputs': {
+            'encoder': encoder_output,
+            'decoder': decoder_output
+        },
+        'states': {
+            'encoder': encoder_final_states,
+            'decoder': decoder_final_states
+        },
+        'layers': {
+            'decoder': {
+                'embedding': decoder_embedding_layer,
+                'last_decoder_lstm': decoder_lstm,
+                'dense': decoder_dense
+            }
+        }
+    }

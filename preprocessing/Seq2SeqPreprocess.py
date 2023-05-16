e752eef91735b94ca6cf805cccc179121fecfb73
ali <a_icam@yahoo.com>
2023-05-16 08:06:03 -0700

    fix: organizing functions in files

---
+++ preprocessing/Seq2SeqPreprocess.py
@@ -0,0 +1,112 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import pickle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+def create_inputs():
+    max_text_len = 42
+    max_summary_len = 13
+    embedding_dim = 300
+
+    df = pd.read_csv('./data/cleaned_data.csv')
+    x_train, x_val, y_train, y_val = train_test_split(
+        np.array(df['text']),
+        np.array(df['summary']),
+        test_size=0.1,
+        random_state=1,
+        shuffle=True
+    )
+    x_tokenizer = pickle.load(open('./data/x_tokenizer', 'rb'))
+    y_tokenizer = pickle.load(open('./data/y_tokenizer', 'rb'))
+
+    # one-hot-encoding
+    x_train_sequence = x_tokenizer.texts_to_sequences(x_train)
+    x_val_sequence = x_tokenizer.texts_to_sequences(x_val)
+
+    # padding upto max_text_len
+    x_train_padded = pad_sequences(x_train_sequence, maxlen=max_text_len, padding='post')
+    x_val_padded = pad_sequences(x_val_sequence, maxlen=max_text_len, padding='post')
+
+    # if you're not using num_words parameter in Tokenizer then use this
+    x_vocab_size = len(x_tokenizer.word_index) + 1
+
+    # else use this
+    # x_vocab_size = x_tokenizer.num_words + 1
+
+    print(x_vocab_size)
+
+    # one-hot-encoding
+    y_train_sequence = y_tokenizer.texts_to_sequences(y_train)
+    y_val_sequence = y_tokenizer.texts_to_sequences(y_val)
+
+    # padding upto max_summary_len
+    y_train_padded = pad_sequences(y_train_sequence, maxlen=max_summary_len, padding='post')
+    y_val_padded = pad_sequences(y_val_sequence, maxlen=max_summary_len, padding='post')
+
+    # if you're not using num_words parameter in Tokenizer then use this
+    y_vocab_size = len(y_tokenizer.word_index) + 1
+
+    # else use this
+    # y_vocab_size = y_tokenizer.num_words + 1
+
+    print(y_vocab_size)
+
+    def remove_indexes(summary_array):
+        remove_indexes = []
+        for i in range(len(summary_array)):
+            count = 0
+            for j in summary_array[i]:
+                if j != 0:
+                    count += 1
+            if count == 2:
+                remove_indexes.append(i)
+        return remove_indexes
+
+    remove_train_indexes = remove_indexes(y_train_padded)
+    remove_val_indexes = remove_indexes(y_val_padded)
+
+    y_train_padded = np.delete(y_train_padded, remove_train_indexes, axis=0)
+    x_train_padded = np.delete(x_train_padded, remove_train_indexes, axis=0)
+
+    y_val_padded = np.delete(y_val_padded, remove_val_indexes, axis=0)
+    x_val_padded = np.delete(x_val_padded, remove_val_indexes, axis=0)
+
+    def get_embedding_matrix(tokenizer, embedding_dim, vocab_size=None):
+        word_index = tokenizer.word_index
+        voc = list(word_index.keys())
+
+        path_to_glove_file = './glove.6B.300d.txt'
+
+        embeddings_index = {}
+        with open(path_to_glove_file) as f:
+            for line in f:
+                word, coefs = line.split(maxsplit=1)
+                coefs = np.fromstring(coefs, "f", sep=" ")
+                embeddings_index[word] = coefs
+
+        print("Found %s word vectors." % len(embeddings_index))
+
+        num_tokens = len(voc) + 2 if not vocab_size else vocab_size
+        hits = 0
+        misses = 0
+
+        # Prepare embedding matrix
+        embedding_matrix = np.zeros((num_tokens, embedding_dim))
+        for word, i in word_index.items():
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                # Words not found in embedding index will be all-zeros.
+                # This includes the representation for "padding" and "OOV"
+                embedding_matrix[i] = embedding_vector
+                hits += 1
+            else:
+                misses += 1
+        print("Converted %d words (%d misses)" % (hits, misses))
+
+        return embedding_matrix
+
+    return x_vocab_size, get_embedding_matrix(x_tokenizer, embedding_dim,
+                                              x_vocab_size), y_vocab_size, get_embedding_matrix(
+        y_tokenizer, embedding_dim, y_vocab_size),
